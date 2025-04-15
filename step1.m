17b0f9b2c1d5c734cfda890efa1447f2543ed665
k-okita08 <kage.182110@icloud.com>
2025-04-15 13:11:24 +0900

    step1の修正

--- step1.m
+++ step1.m
@@ -12,32 +12,35 @@ else
 end
 
 % 窓関数のパラメータ設定
-window_length = 512;
-shift_length = 256;
+windowLength = 512;
+shiftLength = 256;
+
+% 信号の先頭にゼロパディングを追加
+x = [zeros(shiftLength, 1); x];
 
 % ハン窓の実装
-hann_window = 0.5 * (1 - cos(2*pi*(0:window_length-1)'/(window_length-1)));
+hannWindow = 0.5 * (1 - cos(2 * pi * (0:windowLength - 1)' / (windowLength - 1)));
 
 % 信号の長さとフレーム数の計算
-signal_length = length(x);
-num_frames = floor((signal_length - window_length) / shift_length) + 1;
+signalLength = length(x);
+numFrames = floor((signalLength - windowLength) / shiftLength) + 1;
 
 % 短時間信号を格納する配列を初期化
-framed_signal = zeros(window_length, num_frames);
+framedSignal = zeros(windowLength, numFrames);
 
 % 信号を短時間フレームに分割
-for i = 1:num_frames
-    start_idx = (i-1) * shift_length + 1;
-    end_idx = start_idx + window_length - 1;
+for i = 1:numFrames
+    startIdx = (i-1) * shiftLength + 1;
+    endIdx = startIdx + windowLength - 1;
 
     % 窓長を超えないように確認
-    if end_idx <= signal_length
-        frame = x(start_idx:end_idx);
+    if endIdx <= signalLength
+        frame = x(startIdx:endIdx);
     else
         % 信号の終わりに達した場合
-        frame = [x(start_idx:signal_length); zeros(end_idx - signal_length, 1)];
+        frame = [x(startIdx:signalLength); zeros(endIdx - signalLength, 1)];
     end
 
     % 窓関数を適用
-    framed_signal(:, i) = frame(:) .* hann_window;
+    framedSignal(:, i) = frame .* hannWindow;
 end
