6adb7a17ad6dd9ad435cf10d66a82603666898ef
k-okita08 <kage.182110@icloud.com>
2025-04-22 16:51:40 +0900

    step3修正2回目

--- main.m
+++ main.m
@@ -14,60 +14,62 @@ else
 end
 
 % 窓関数のパラメータ設定
-windowLength = 512;
-shiftLength = 256;
+winLen = 512;
+shihtLen = 256;
 
 % FFTのパラメータ設定
-nfft = windowLength;
+nfft = winLen;
 
 % 信号の先頭にゼロパディングを追加
-x = [zeros(windowLength / 2, 1); x];
+x = [zeros(winLen / 2, 1); x];
 
 % ハン窓の実装
-hannWindow = 0.5 * (1 - cos(2 * pi * (0:windowLength - 1)' / (windowLength - 1)));
+hannWindow = 0.5 * (1 - cos(2 * pi * (0:winLen - 1)' / (winLen - 1)));
 
 % 信号の長さとフレーム数の計算
-signalLength = length(x);
-numFrames = floor(signalLength / shiftLength);
+sigLen = length(x);
+numFrames = floor(sigLen / shihtLen);
 
 % 複素スペクトログラム格納用配列
-complexSpectrogram = zeros(nfft, numFrames);
+complexSpec = zeros(nfft, numFrames);
+
+powerSpec_dB = zeros(nfft, numFrames);
 
 % 信号を短時間フレームに分割
 for i = 1:numFrames
-    startIdx = (i-1) * shiftLength + 1;
-    endIdx = startIdx + windowLength - 1;
+    startIdx = (i-1) * shihtLen + 1;
+    endIdx = startIdx + winLen - 1;
 
     % 窓長を超えないように確認
-    if endIdx <= signalLength
+    if endIdx <= sigLen
         frame = x(startIdx:endIdx);
     else
         % 信号の終わりに達した場合
-        frame = [x(startIdx:signalLength); zeros(endIdx - signalLength, 1)];
+        frame = [x(startIdx:sigLen); zeros(endIdx - sigLen, 1)];
     end
 
     % 窓関数を適用
-    framedSignal = frame .* hannWindow;
+    framedsig = frame .* hannWindow;
 
     % 窓かけ済み信号をFFT
-    spectrum = fft(framedSignal, nfft, 1);
+    spec = fft(framedsig, nfft, 1);
 
     % 複素スペクトログラムに格納
-    complexSpectrogram(:, i) = spectrum;
+    complexSpec(:, i) = spec;
 
     % パワースペクトログラム
-    powerSpectrogram = abs(spectrum).^2;
+    powerSpec = abs(spec).^2;
 
     % dBスケールに変換
-    powerSpectrogram_dB(:, i) = 10 * log10(powerSpectrogram);
+    powerSpec_dB(:, i) = 10 * log10(powerSpec);
 end
 
 % 時間軸と周波数軸の生成
-time = (0:numFrames-1) * shiftLength / Fs; % 秒
+time = (0:numFrames-1) * shihtLen / Fs; % 秒
 freq = (0:nfft-1) * Fs / nfft;             % Hz
 
 % --- パワースペクトログラムの描画 ---
-imagesc(time, freq, powerSpectrogram_dB);
+imagesc(time, freq, powerSpec_dB);
 axis xy;
 colormap(jet);
 ylim([0 Fs/2]);
